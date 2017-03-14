911528fa633e543b6aeaf7320dea49a3910b0786
Wood, Christopher <CJW@stowers.org>
2017-03-14 15:31:20 -0500

    write pixels to results table

--- src/main/java/org/stowers/microscopy/ij1plugins/PixelsToText.java
+++ src/main/java/org/stowers/microscopy/ij1plugins/PixelsToText.java
@@ -3,6 +3,7 @@ package org.stowers.microscopy.ij1plugins;
 /**
  * Created by cjw on 3/13/17.
  */
+import java.awt.*;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
@@ -14,14 +15,15 @@ import java.util.ArrayList;
 
 import ij.*;
 import ij.gui.MessageDialog;
+import ij.gui.Roi;
 import ij.io.FileInfo;
+import ij.measure.ResultsTable;
+import ij.process.ImageProcessor;
 import net.imagej.*;
 import org.scijava.command.Command;
 import org.scijava.command.Previewable;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-
-import java.awt.Frame;
 
 @Plugin(type = Command.class, name = "Click for Roi",  menuPath="Plugins>Stowers>Chris>Pixels To Text")
 public class PixelsToText implements Command, Previewable {
@@ -29,25 +31,87 @@ public class PixelsToText implements Command, Previewable {
     @Parameter
     ImagePlus imp;
 
-    @Parameter(label="Select Ouput Directory", style="directory")
+    @Parameter(label="Put in results table?", callback = "resultsChanged")
+    boolean inResults;
+
+    @Parameter(required=false, label="Select Ouput Directory", style="directory")
     File outputDir;
 
-
+    String strDir;
 
     @Override
     public void run() {
 
-        Charset charset = Charset.forName("US-ASCII");
+
         String name = imp.getTitle();
-        Path outputFile = Paths.get(outputDir + "/" + name + ".txt");
+        Path outputFile = null;
+        if (!outputDir.exists() || outputDir == null) {
+            inResults = true;
+        } else {
+
+            outputFile = Paths.get(outputDir + "/" + name + ".txt");
+        }
+
         float[] pixels = (float[])(imp.getProcessor().convertToFloatProcessor().getPixels());
 
+        float[] outPixels;
+        if (imp.getRoi() != null) {
+            Point[] points = imp.getRoi().getContainedPoints();
+            outPixels = new float[points.length];
+            int i = 0;
+            for (Point p : points) {
+                int x = (int)p.getX();
+                int y = (int)p.getY();
+                int index = imp.getWidth()*y + x;
+                outPixels[i] = pixels[index];
+                i++;
+            }
+            System.out.println(points.length);
+        } else {
+            outPixels = pixels;
+        }
+
+        if (inResults) {
+            writeResults(outPixels);
+        } else {
+            writePixels(outputFile, outPixels);
+        }
+
+    }
+
+    protected void resultsChanged() {
+
+        if (inResults == true) {
+            //strDir = outputDir.getAbsolutePath();
+            outputDir = null;
+        }
+        else {
+            outputDir = new File(strDir);
+        }
+    }
+
+    private void writeResults(float[] outPixels) {
+
+        ResultsTable table = new ResultsTable();
+
+        for (int i = 0; i < outPixels.length; i++) {
+            table.incrementCounter();
+            float pix = outPixels[i];
+            String pout = String.format("%10.2f\n" , pix);
+            table.addValue("Value", pix);
+        }
+        table.show("Pixel Values");
+    }
+
+    private void writePixels(Path outputFile, float[] outPixels) {
+
+        Charset charset = Charset.forName("US-ASCII");
         try {
             BufferedWriter writer = Files.newBufferedWriter(outputFile, charset);
 
             String pout;
-            for (int i = 0; i < pixels.length; i++) {
-                float pix = pixels[i];
+            for (int i = 0; i < outPixels.length; i++) {
+                float pix = outPixels[i];
                 pout = String.format("%10.2f\n" , pix);
                 writer.write(pout);
             }
@@ -57,10 +121,7 @@ public class PixelsToText implements Command, Previewable {
         catch (IOException e) {
             IJ.log("Can't create the output file");
         }
-
-
     }
-
     @Override
     public void preview() {
 
